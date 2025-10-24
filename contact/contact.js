24d5b3d49cc5d2a7c6e39b1e8d69aac0c5b89a16
adeogunb <jamiuadeogun715@gmail.com>
2025-10-25 00:48:23 +0100

    final version

--- contact/contact.js
+++ contact/contact.js
@@ -3,14 +3,15 @@ const emailInput = document.getElementById('email')
 const subjectInput = document.getElementById('subject')
 const messageInput = document.getElementById('message')
 const contactPage = document.querySelector('.js-contact-page')
-const popUpButton = document.querySelector('.js-pop-up-button')
+
+
 
 function renderContactForm(){
   contactPage.innerHTML=
     `<h1>Contact Us</h1>
       <p>Please take a moment to get in touch, we will get back to you shortly</p>
 
-      <form action="">
+      <form action="" id="js-contact-form">
         <fieldset>
           <label for="name">*Full name:</label>
           <input type="text"  id="name" data-testid="test-contact-name" required>      
@@ -34,38 +35,58 @@ function renderContactForm(){
         
       </form>
     `
-
-}
-
-function renderErrorMessage(){
-   contactPage.innerHTML=
+    function renderErrorMessage(error){
+   contactPage.innerHTML+=
    ` <section data-testid="test-contact-error-email" class="message js-error-message">
-        <p class="pop-up-message">An error Occured!!</p>
+        <p class="pop-up-message">${error}An error Occured!!</p>
         <div class="pop-up-button js-pop-up-button">Try again!</div>
 
       </section>
     `
+    const popUpButton = document.querySelector('.js-pop-up-button')
+    popUpButton.addEventListener('click', ()=>{
+   renderContactForm()
+      })
 
 }
 
 function renderSuccessMessage(){
-  contactPage.innerHTML=
+  contactPage.innerHTML+=
   ` <section data-testid="test-contact-success" class="message js-success-message">
         <p class="pop-up-message">Thanks for your submission</p>
         <div class="pop-up-button js-pop-up-button">OK!</div>
 
       </section>
    `
+   const popUpButton = document.querySelector('.js-pop-up-button')
+   popUpButton.addEventListener('click', ()=>{
+   renderContactForm()
+      })
+}
+
+
+
+
+document.getElementById("js-contact-form").addEventListener('submit', (event)=>{
+  try {
+    event.preventDefault();
+    renderSuccessMessage()
+
+  } catch (error) {
+    renderErrorMessage(error)
+
+    }
+})
+
+
 }
 renderContactForm()
 
-document.getElementById("submit").addEventListener('click', ()=>{
-  try {
-    if(!emailInput.){
-      console.log("error")
-    } else {console.log('success')}
+
+
+
+
+
+
     
-  } catch (error) {
-    
-  }
-})
+
